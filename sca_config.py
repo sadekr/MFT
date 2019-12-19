bfd478d7203974706f61f1f624d696a750306306
sadekr <rita.sadek@cern.ch>
2019-12-19 15:25:03 +0100

    Start of MP/SM/GUI implementation

--- sca_config.py
+++ sca_config.py
@@ -1,10 +1,64 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3.8
 import sys
 import argparse
-
 import SCA 
 from SCA import Sca
+import multiprocessing
+import time
+from tkinter import *
+from multiprocessing import shared_memory
+import ctypes
+import numpy as np
+
+class GuiApp(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)                                                                
+        self.pack()
+
+    def print_contents(self, event):
+        print("Entry content is: ", \
+              self.contents.get())
+
+    def read_value(self, mat):
+        print(mat.shape)
+        label = Label(root, text= mat[1][0], width=20, font=("bold",10))
+        label.place(x=70,y=130)
+
+    def read_value1(self, mat):
+        label = Label(root, text= mat[1][2], width=20, font=("bold",10))
+        label.place(x=70,y=200)
+    
+    def change_value(self, mat, sq):
+        mat[1][0] = 55555
+        print(mat[1][0],sq[1][0])
+        print(mat,sq)
+
+    def createWidgets(self, sq):                                      
+        print(" In createWidgers ")
+        QUIT = Button(self, fg= "red", text = "Q", command = self.quit)
+        QUIT.pack(side = "right")                               
+        self.entrythingy = Entry()
+        self.entrythingy.pack()
+        self.contents = StringVar()
+        self.contents.set("Enter variable")                                                      
+        self.entrythingy["textvariable"] = self.contents
+        self.entrythingy.bind('<Key-Return>',
+                              self.print_contents)
+
+        existing_shm=shared_memory.SharedMemory(name='SM1')
+        mat=np.ndarray(sq.shape, dtype=sq.dtype, buffer=existing_shm.buf)
+ 
+        button1=Button(self, text=" Value of mat[1][0] ", command = lambda : self.read_value(mat))
+        button1.pack()
+        button3=Button(self, text=" Value of mat[1][2] ", command = lambda : self.read_value1(mat))
+        button3.pack()
+        button2=Button(self, text=" Change mat[1][0] ", command = lambda : self.change_value(mat,sq))
+        button2.pack()
+        self.mainloop()
+
+        del mat
+        existing_shm.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -39,10 +93,48 @@ def main():
     #sys.stdout = dcsLogFile
     
     sca.MidCfgFile(dcsFileName)
-    sca.MFTEN_load(debug)
-    sca.MFTEN_execute(debug, k)
+    mat=sca.MFTEN_load(debug)
+    sca.MFTEN_execute_cmd(debug, mat)
+    global h
+    if k==1 :
+        h=True
     #dcsLogFile.close()
     sys.stdout = stdoutBackup
 
+def sequence_loop(sq):
+    existing_shm=shared_memory.SharedMemory(name='SM1')
+    mat=np.ndarray(sq.shape, dtype=sq.dtype, buffer=existing_shm.buf)
+    while(h)
+         sca.MFTEN_execute_cmd(debug,mat)#
+    del mat
+    existing_shm.close()
+
 if __name__ == '__main__' :
     main()
+
+    if h :
+        sca= Sca(id_card, 2, gbt_ch, board, debug) ##
+
+        root = Tk()
+        root.geometry('500x500')
+        gui = GuiApp(master=root)
+
+        mat=sca.MFTEN_load(debug) 
+        mat=np.array(mat)
+
+        shm= shared_memory.SharedMemory(name='SM1', create=True, size=mat.nbytes)
+        sq=np.ndarray(mat.shape, dtype=mat.dtype, buffer=shm.buf) 
+        sq[:]=mat[:]
+
+        t1=multiprocessing.Process(target=gui.createWidgets, args=(sq,))
+        t1.start()
+        t2=multiprocessing.Process(target=sequence_loop, args=(sq,))
+        t2.start()
+        t1.join()
+        t2.join()
+
+        del sq
+        shm.close()
+        shm.unlink()
+
+
